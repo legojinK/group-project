c70961db97000b0eb0afa3762d77662491a0f70b
heejinkim <hjkim2@okestro.com>
2024-09-09 16:18:53 +0900

    first commit

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+// 절대 경로 설정
+module.exports = {
+    webpack: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@model': path.resolve(__dirname, 'src/model'),
+            '@api': path.resolve(__dirname, 'src/api'),
+            '@store': path.resolve(__dirname, 'src/store'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@containers': path.resolve(__dirname, 'src/containers'),
+        },
+    },
+};
