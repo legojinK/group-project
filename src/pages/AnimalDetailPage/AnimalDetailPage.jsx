3ef4343c9d0dc458735998ff00d833258f123281
Suki <tues1205@naver.com>
2024-09-13 01:30:44 +0900

    nothing

--- src/pages/AnimalDetailPage/AnimalDetailPage.jsx
+++ src/pages/AnimalDetailPage/AnimalDetailPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+// import React from "react";
 
-const AnimalDetailPage = () => {
-  return <div>AnimalDetailPage</div>;
-};
+// import { useParams } from "react-router-dom";
+// import { useAnimalDetailDataQuery } from "@/hooks/useAnimalDetailData";
+// import { Spinner, Alert } from "react-bootstrap";
 
-export default AnimalDetailPage;
+// const AnimalDetailPage = () => {
+//   const id = useParams();
+//   const { data, isLoading, isError, error } = useAnimalDetailDataQuery(id);
+//   console.log("ddd", data);
+
+//   if (isLoading) {
+//     // 로딩스피너
+//     return (
+//       <div className="container">
+//         <Spinner
+//           animation="border"
+//           variant="info"
+//           style={{ width: "3rem", height: "3rem" }}
+//         />
+//       </div>
+//     );
+//   }
+//   if (isError) {
+//     // 에러 메세지
+//     return <Alert variant="danger">{error.message}</Alert>;
+//   }
+
+//   return <div>{data?.age}</div>;
+// };
+
+// export default AnimalDetailPage;
