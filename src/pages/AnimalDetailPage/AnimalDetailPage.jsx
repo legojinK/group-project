770da8c835ad2679bdfc8c2500942d555883e501
Suki <tues1205@naver.com>
2024-09-14 01:00:10 +0900

    AnimalDetailPage css

--- src/pages/AnimalDetailPage/AnimalDetailPage.jsx
+++ src/pages/AnimalDetailPage/AnimalDetailPage.jsx
@@ -1,53 +1,120 @@
 import React from "react";
+import "./AnimalDetailPage.style.css";
+import { Container } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 
-// import { useParams } from "react-router-dom";
-// import { useAnimalDetailDataQuery } from "@/hooks/useAnimalDetailData";
-// import { Spinner, Alert } from "react-bootstrap";
+// 날짜 '20240913' -> '2024년 09월 13일'
+const formatDate = (dateString) => {
+  if (!dateString || dateString.length !== 8) return "날짜 정보 없음";
+
+  const year = dateString.slice(0, 4);
+  const month = dateString.slice(4, 6);
+  const day = dateString.slice(6, 8);
+
+  return `${year}년 ${month}월 ${day}일`;
+};
 
 const AnimalDetailPage = () => {
   const location = useLocation();
   const animal = location.state?.animal; // 상태객체에서 animal 가져오기
-  // const {
-  //   state: {
-  //     animal: {
-  //       desertionNo,
-  //       filename,
-  //       happenDt,
-  //       happenPlace,
-  //       // kindCd,
-  //       // colorCd,
-  //       age,
-  //       // weight,
-  //       // sexCd,
-  //     },
-  //   },
-  // } = useLocation();
-  //   const id = useParams();
-  //   const { data, isLoading, isError, error } = useAnimalDetailDataQuery(id);
-  //   console.log("ddd", data);
-
-  //   if (isLoading) {
-  //     // 로딩스피너
-  //     return (
-  //       <div className="container">
-  //         <Spinner
-  //           animation="border"
-  //           variant="info"
-  //           style={{ width: "3rem", height: "3rem" }}
-  //         />
-  //       </div>
-  //     );
-  //   }
-  //   if (isError) {
-  //     // 에러 메세지
-  //     return <Alert variant="danger">{error.message}</Alert>;
-  //   }
 
   return (
-    <div>
-      {animal.desertionNo},{animal.happenDt},{animal.happenPlace}, {animal.age}
-      <img src={animal.filename} alt="animal-photo" />
+    <div className="animal-detail">
+      <div className="animals-page-title">
+        <span>보호 중인 동물</span>
+      </div>
+
+      <Container>
+        {/* 동물 사진 */}
+        <div className="animal-info">
+          <img
+            src={animal?.popfile}
+            alt="animal-photo"
+            className="animal-photo"
+          />
+
+          {/* 동물 정보들 */}
+
+          <table border="1">
+            <tbody>
+              <tr>
+                <th>공고번호</th>
+                <td>{animal?.noticeNo}</td>
+              </tr>
+              <tr>
+                <th>유기번호</th>
+                <td>{animal?.desertionNo}</td>
+              </tr>
+              <tr>
+                <th>품종</th>
+                <td>{animal?.kindCd}</td>
+              </tr>
+              <tr>
+                <th>색상</th>
+                <td>{animal?.colorCd}</td>
+              </tr>
+              <tr>
+                <th>성별</th>
+                <td>
+                  {animal?.sexCd === "M"
+                    ? "수컷"
+                    : animal?.sexCd === "F"
+                    ? "암컷"
+                    : "미확인"}
+                </td>
+              </tr>
+              <tr>
+                <th>중성화</th>
+                <td>
+                  {animal?.neuterYn === "Y"
+                    ? "예"
+                    : animal?.neuterYn === "N"
+                    ? "아니오"
+                    : "미확인"}
+                </td>
+              </tr>
+              <tr>
+                <th>나이/체중</th>
+                <td>
+                  {animal?.age} / {animal?.weight}
+                </td>
+              </tr>
+              <tr>
+                <th>특징</th>
+                <td>{animal?.specialMark}</td>
+              </tr>
+              <tr>
+                <th>발견장소</th>
+                <td>{animal?.happenPlace}</td>
+              </tr>
+              <tr>
+                <th>접수일시</th>
+                <td>{formatDate(animal?.happenDt)}</td>
+              </tr>
+              <tr>
+                <th>관할기관</th>
+                <td>{animal?.orgNm}</td>
+              </tr>
+              <tr>
+                <th>상태</th>
+                <td>{animal?.processState}</td>
+              </tr>
+              <tr>
+                <th>보호센터</th>
+                <td>{animal?.careNm}</td>
+              </tr>
+              <tr>
+                <th>연락처</th>
+                <td>{animal?.careTel}</td>
+              </tr>
+              <tr>
+                <th>보호장소</th>
+                <td>{animal?.careAddr}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </Container>
     </div>
   );
 };
