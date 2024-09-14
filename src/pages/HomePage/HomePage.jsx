aa88a28ae8f7bf03aaecd0996358ca7940c6a398
Suki <tues1205@naver.com>
2024-09-14 20:05:34 +0900

    Aside

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Banner from "./component/Banner/Banner";
 import "./HomePage.style.css";
 import HomeAnimalSearch from "./component/HomeAnimalSearch/HomeAnimalSearch";
+import Information from "./component/Information/Information";
 
 const HomePage = () => {
   return (
     <div className="home-area">
       <Banner />
       <HomeAnimalSearch />
+      <Information />
     </div>
   );
 };
---
+++ src/pages/HomePage/component/Information/Information.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Information.style.css";
+import URLCard from "../../../../common/URLCard/URLCard";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Information = () => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} lg={6}>
+          <div className="info-urlcard">
+            <URLCard
+              title={"동물사랑배움터"}
+              content={
+                "동물 관련 영업자, 명예감시원, 맹견소유자는 연 1회 법정의무교육을 이수해야 합니다."
+              }
+              urlPath={"https://apms.epis.or.kr/home/kor/main.do"}
+            />
+          </div>
+        </Col>
+        <Col xs={12} lg={6}>
+          <div className="info-call">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/10439/10439798.png"
+              alt="info-img"
+            />
+            <div className="info-info">
+              <div className="info-text">동물보호 상담센터</div>
+              <div className="info-tel">1577-0954</div>
+              <div className="info-text">
+                시스템 문의: 054-810-8626 <br />
+                상담시간: 평일 09:00 ~ 18:00
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Information;
