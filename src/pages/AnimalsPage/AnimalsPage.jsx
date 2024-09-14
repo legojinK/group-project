8bfab24102395e5560ce2b2d898dfa24a7e3d8a3
kimsh0306 <b666790@gmail.com>
2024-09-14 21:43:05 +0900

    AnimalsPage CSS

--- src/pages/AnimalsPage/AnimalsPage.jsx
+++ src/pages/AnimalsPage/AnimalsPage.jsx
@@ -5,22 +5,37 @@ import AnimalCardWrapper from "./component/AnimalCardWrapper/AnimalCardWrapper";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import Pagination from "./component/pagination/Pagination";
+import { Container } from "react-bootstrap";
 
 const AnimalsPage = () => {
   return (
     <div className="animals-page">
-      <div className="animals-page-title">
-        <span>보호 중인 동물</span>
+      <h6 className="page-nav">
+        구조동물 &nbsp; &gt; &nbsp; <strong>보호 중인 동물</strong>
+      </h6>
+      <div className="animals-img-box">
+        <img
+          className="img"
+          src="https://www.animal.go.kr/front/images/contents/headline_img3.png"
+        />
       </div>
+
+      {/* <div className="animals-page-title">
+        <span>보호 중인 동물</span>
+      </div> */}
+
       <div className="animals-page-content">
-        <div className="animals-page-instruction-box">
-          <FontAwesomeIcon
-            icon={faInfoCircle}
-            className="animals-page-info-icon"
-          />
-          동물 입양상담은 동물보호센터로 문의하십시오.
-        </div>
-        <AnimalFilter />
+        <Container>
+          <h1 className="content-title">상세 검색</h1>
+          <AnimalFilter />
+          <div className="animals-page-instruction-box">
+            <FontAwesomeIcon
+              icon={faInfoCircle}
+              className="animals-page-info-icon"
+            />
+            동물 입양상담은 동물보호센터로 문의하십시오.
+          </div>
+        </Container>
         <AnimalCardWrapper />
         <Pagination />
       </div>
